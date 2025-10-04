46b44a8acd366b5f38a97411745a3762b8825cb1
Sreynet-long <sreynetlong168@gmail.com>
2025-10-04 17:19:48 +0700

    test

---
+++ app/lib/apolloClient.js
@@ -0,0 +1,23 @@
+'use client'
+import {ApolloProvider} from "@apollo/client/react";
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+
+export function createApolloClient(){
+    return new ApolloClient({
+        link: new HttpLink({
+            uri: "http://localhost:4000/graphql", //Replace your endpoint (api link) here!
+            fetch
+        }),
+        cache: new InMemoryCache()
+    })
+}
+
+const client = createApolloClient()
+
+export function ApolloWrapper({children}){
+    return (
+        <ApolloProvider client={client}>
+            {children}
+        </ApolloProvider>
+    )
+}
