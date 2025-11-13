64ca29a9da2162bb15cac9220180cb8b87acf390
Sreynet-long <sreynetlong168@gmail.com>
2025-11-13 16:24:23 +0700

    11/13/25

--- app/lib/apolloClient.js
+++ app/lib/apolloClient.js
@@ -5,7 +5,8 @@ import {ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 export function createApolloClient(){
     return new ApolloClient({
         link: new HttpLink({
-            uri: " http://localhost:6380/graphql", //Replace your endpoint (api link) here!
+            // uri: " http://localhost:6380/graphql",
+            uri: "https://freshmart-backend-b73r.onrender.com" 
             fetch
         }),
         cache: new InMemoryCache()
